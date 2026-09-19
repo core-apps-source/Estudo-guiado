f972ec7ee8085907deac148387d969fe441e611d
Matheus <matheusjuliano22icm@gmail.com>
2026-09-19 12:12:05 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,9 +4,9 @@ const SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/icon-512-maskable.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-512-maskable.png",
 ];
 
 self.addEventListener("install", (event) => {
